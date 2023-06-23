9581af4466c28e0069fd86a50eb65d2e583b06ee
dudcks5477 <dudcks5477@naver.com>
2023-06-23 16:47:37 +0900

    Add Styles folder

---
+++ Frontend/src/styles/SearchBarStyle.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const searchBarStyle = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    borderRadius: 13,
+    marginTop: 46,
+    marginLeft: 14,
+    marginRight: 14,
+    backgroundColor: '#D2D2D2',
+  },
+});
