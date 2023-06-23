a3f4e52f24ff9462a0c53b15ecc6167cbfca61ac
dudcks5477 <dudcks5477@naver.com>
2023-06-23 14:58:26 +0900

    SearchBar Component

---
+++ Frontend/src/component/SearchBar.js
@@ -0,0 +1,31 @@
+import React, {ustState} from 'react';
+import {View, TextInput} from 'react-native';
+
+export default function SearchBar() {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    console.log(query);
+  };
+
+  return (
+    <View style={{flexDirection: 'row'}}>
+      <TextInput
+        style={{
+          flex: 1,
+          height: 56,
+          borderRadius: 13,
+          marginTop: 46,
+          marginLeft: 14,
+          marginRight: 14,
+          backgroundColor: '#D2D2D2',
+        }}
+        value={query}
+        onChangeText={setQuery}
+        placeholder='언제든 파티 찾기'
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
+    </View>
+  )
+}
