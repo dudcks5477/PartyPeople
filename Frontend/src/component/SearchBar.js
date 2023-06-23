56273d22dd2c50388d0e463f064f2a12e7c78e32
dudcks5477 <dudcks5477@naver.com>
2023-06-23 16:49:10 +0900

    Search bar component

--- Frontend/src/component/SearchBar.js
+++ Frontend/src/component/SearchBar.js
@@ -1,31 +1,20 @@
-import React, {ustState} from 'react';
+import React, {useState} from 'react';
 import {View, TextInput} from 'react-native';
+import {searchBarStyle} from './searchBarStyle';
 
 export default function SearchBar() {
   const [query, setQuery] = useState('');
 
-  const handleSearch = () => {
-    console.log(query);
-  };
-
   return (
-    <View style={{flexDirection: 'row'}}>
+    <View style={searchBarStyle.container}>
       <TextInput
-        style={{
-          flex: 1,
-          height: 56,
-          borderRadius: 13,
-          marginTop: 46,
-          marginLeft: 14,
-          marginRight: 14,
-          backgroundColor: '#D2D2D2',
-        }}
+        style={searchBarStyle.input}
         value={query}
-        onChangeText={setQuery}
-        placeholder='언제든 파티 찾기'
-        autoCapitalize='none'
+        onChange={setQuery}
+        placeholder="언제든 파티 찾기"
+        autoCapitalize="none"
         autoCorrect={false}
       />
     </View>
-  )
+  );
 }
