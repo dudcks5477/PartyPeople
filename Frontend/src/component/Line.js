314502055bef386c8aea2de01fe6b4e4e24ebf3c
dudcks5477 <dudcks5477@naver.com>
2023-06-23 16:48:30 +0900

    Line component

--- Frontend/src/component/Line.js
+++ Frontend/src/component/Line.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
-const Line = ({ style }) => {
+const Line = ({style}) => {
   return <View style={[styles.line, style]} />;
 };
 
