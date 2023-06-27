fdf2625821cc2cb9fad93b4054116c06c1baf41a
dudcks5477 <dudcks5477@naver.com>
2023-06-27 13:58:06 +0900

    Add SearchBar component

---
+++ Frontend/src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import {View, TextInput} from 'react-native';
+
+export default function SearchBar() {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    // 검색 버튼을 눌렀을 때 처리할 코드
+    console.log(query);
+  };
+
+  return (
+    <View style={{flexDirection: 'row'}}>
+      <TextInput
+        style={{
+          flex: 1,
+          height: 56,
+          borderRadius: 13,
+          marginTop: 46,
+          marginLeft: 14,
+          marginRight: 14,
+          backgroundColor: '#D2D2D2',
+        }}
+        value={query}
+        onChangeText={setQuery}
+        placeholder="언제든 파티 찾기"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+    </View>
+  );
+}
