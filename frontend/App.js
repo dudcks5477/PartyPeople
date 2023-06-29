b45df6d33a4ab0420a56f5b740e0daeed494e82c
dudcks5477 <dudcks5477@naver.com>
2023-06-29 16:51:41 +0900

    Login Design & Style

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import LoginScreen from './src/screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => (
   <NavigationContainer>
     <Stack.Navigator>
-      
+      <Stack.Screen 
+        name="Login" 
+        component={LoginScreen} 
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack.Navigator>
   </NavigationContainer>
 );
---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,91 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text, Keyboard, TextInput, TouchableOpacity} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {styles} from '../styles/LoginScreenStyle';
+
+export default function LoginScreen() {
+  const [keyboardStatus, setKeyboardStatus] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const keyboardDidshowListener = Keyboard.addListener(
+      'keyboardDidshow',
+      () => {
+        setKeyboardStatus(true);
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setKeyboardStatus(false);
+      }
+    );
+
+    return () => {
+      keyboardDidshowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
+  const navigation = useNavigation();
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://3.35.21.149:8080/users/login', {
+        email: email,
+        password: password,
+      });
+
+      const userId = response.data.id;
+
+      await AsyncStorage.setItem('useId', JSON.stringify(userId));
+
+      console.log('a', userId);
+
+      navigation.navigate('BottomTab', {screen: 'Home'});
+    } catch(error) {
+      console.error(error);
+    }
+  };
+
+  const handleRegister = () => {
+    navigation.navigate('Register');
+  };
+
+  
+  return (
+    <View style={styles.container}>
+      {!keyboardStatus && 
+        <Text style={styles.loginText}>로그인</Text>
+      }
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder='이메일'
+          value={email}
+          onChange={setEmail}
+          autoCapitalize='none'
+          keyboardType='email-address'
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='비밀번호'
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+      </View>
+      
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>로그인</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handleRegister}>
+        <Text style={styles.buttonText}>회원가입</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
