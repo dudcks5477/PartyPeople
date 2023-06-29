d43c9b87e397ddadc387a0607e5d443068ee7f5f
dudcks5477 <dudcks5477@naver.com>
2023-06-29 14:42:10 +0900

    Experiment

--- frontend/App.js
+++ frontend/App.js
@@ -1,40 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-// import LoginScreen from './src/screens/LoginScreen.js';
-// import HomeScreen from './src/screens/HomeScreen.js';
-// import UnderBarButton from './src/container/UnderBarButton.js';
-import MapScreen from './src/screens/MapScreen.js';
+
 const Stack = createNativeStackNavigator();
 
 const App = () => (
   <NavigationContainer>
     <Stack.Navigator>
-      {/* <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="HomeSearchBar"
-        component={HomeScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="BottomTab"
-        component={UnderBarButton}
-        options={{
-          headerShown: false,
-        }}
-      /> */}
-      <Stack.Screen
-        name="MapScreen"
-        component={MapScreen} 
-      />
+      
     </Stack.Navigator>
   </NavigationContainer>
 );
