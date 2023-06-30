e832e0ebfdeab30d4879ab17559dff8b94ee1b43
dudcks5477 <dudcks5477@naver.com>
2023-06-30 15:11:20 +0900

    Add Home Screen Stack Screen

--- frontend/App.js
+++ frontend/App.js
@@ -2,15 +2,23 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => (
   <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen 
+      {/* <Stack.Screen 
         name="Login" 
         component={LoginScreen} 
+        options={{
+          headerShown: false,
+        }}
+      /> */}
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
         options={{
           headerShown: false,
         }}
