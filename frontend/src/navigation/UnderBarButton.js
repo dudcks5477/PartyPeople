affe67a05822c409a2f1ad9cb4450d25a26b0f3d
dudcks5477 <dudcks5477@naver.com>
2023-06-30 16:10:14 +0900

    Add Under bar button

---
+++ frontend/src/navigation/UnderBarButton.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import HomeScreen from '../screens/HomeScreen';
+
+const Tab = createBottomTabNavigator();
+
+const UnderBarButton = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, size}) => {
+          const icons = {
+            Map: 'map',
+            wishilist: 'favorite',
+            Home: 'home',
+            Chat: 'chat',
+            PrivateProfile: 'person',
+          };
+          const iconName = icons[route.name];
+          return (
+            <MaterialIcons
+              name={iconName}
+              color={focused ? '#B39DDB' : 'white'}
+              size={size}
+            />
+          );
+        },
+        tabBarActiveTintColor: '#B39DDB',
+        tabBarInactiveTintColor: 'white',
+        tabBarStyle: { backgroundColor: 'black' }
+      })}>
+      {/* <Tab.Screen 
+      name="Map" 
+      component={MapScreen}
+      options={{
+        headerShown: false,
+      }}
+    />
+    <Tab.Screen
+      name="Wishilist"
+      component={WishilistScreen}
+      options={{
+        headerShown: false,
+      }}
+    /> */}
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      {/* <Tab.Screen
+      name="Chat"
+      component={ChatScreen}
+      options={{
+        headerShown: false,
+      }}
+    />
+    <Tab.Screen
+      name="PrivateProfile"
+      component={PrivateProfile}
+      options={{
+        headerShown: false,
+      }}
+    />   */}
+    </Tab.Navigator>
+  );
+};
+
+export default UnderBarButton;
