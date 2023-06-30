9d273b8268d2de22032cdc489ced5dd2237b5ca4
dudcks5477 <dudcks5477@naver.com>
2023-06-30 15:13:06 +0900

    Delete gesture package

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
-import {View} from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import {View, TextInput} from 'react-native';
 
 export default function SearchBar() {
   const [query, setQuery] = useState('');
