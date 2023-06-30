d7a75bbdd381cb418a65268c5b71c2d17207b782
dudcks5477 <dudcks5477@naver.com>
2023-06-30 15:13:40 +0900

    Should we change this Screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,90 @@
-import React from 'react';
-import {View} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import {styles} from '../styles/HomeScreenStyle';
-import {SearchBar} from '../components/SearchBar';
-import {Line} from '../components/Line';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, StyleSheet, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import SearchBar from '../components/SearchBar';
+import Line from '../components/Line';
+import Card from '../components/Card';
 
-export default function HomeScreen() {
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  const [partyData, setPartyData] = useState(null);
+  const [showHeader, setShowHeader] = useState(true);
 
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const partyData = await AsyncStorage.getItem('partyData');
+        if (partyData !== null) {
+          setPartyData(JSON.parse(partyData));
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
 
+    getData();
+  }, []);
+
+  // 스크롤 50 이상하면 searchBar랑 Line 사라지는거
+  const handleScroll = (event) => {
+    const currentOffset = event.nativeEvent.contentOffset.y;
+    if (currentOffset > 50) {
+      setShowHeader(false);
+    } else {
+      setShowHeader(true);
+    }
+  };
+
+  // const clearAllData = async () => {
+  //   try {
+  //     await AsyncStorage.clear();
+  //     console.log('AsyncStorage cleared successfully');
+  //   } catch (e) {
+  //     console.log('Failed to clear AsyncStorage:', e);
+  //   }
+  // };
+  
   return (
     <View style={styles.container}>
-      <SafeAreaView style={styles.containerNotch}>
-        <View style={styles.containerSearch}>
+      {showHeader && (
+        <>
           <SearchBar />
           <Line style={{ marginTop: 20 }} />
+        </>
+      )}
+      <SafeAreaView style={styles.containerNotch}>
+        <View style={styles.containerParty}>
+          <ScrollView onScroll={handleScroll}>
+            {partyData && (
+              <TouchableOpacity
+                style={styles.cardButton}
+                onPress={() => navigation.navigate('PartyDetail')}
+                activeOpacity={1}>
+                <Card partyData={partyData}/>
+              </TouchableOpacity>
+            )}
+          </ScrollView>
+        </View>
+        <View>
+          {/* <Button title="Clear AsyncStorage" onPress={clearAllData} /> */}
         </View>
       </SafeAreaView>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#222',
+  },
+  containerParty: {
+    marginVertical: 10,
+  },
+  cardButton: {
+    marginBottom: 10,
+  },
+});
+
+export default HomeScreen;
