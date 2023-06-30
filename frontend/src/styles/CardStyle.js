fd3bff513544f00e0d9461bce812b8ea2593c7a0
dudcks5477 <dudcks5477@naver.com>
2023-06-30 15:14:26 +0900

    Add Card style

---
+++ frontend/src/styles/CardStyle.js
@@ -0,0 +1,32 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  cardContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 16,
+    overflow: 'hidden',
+    elevation: 5,
+    margin: 10,
+    width: '90%',
+    aspectRatio: 1.5,
+  },
+  image: {
+    width: '100%',
+    heigth: '60%',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginHorizontal: 10,
+    marginTop: 10,
+  },
+  description: {
+    fontSize: 14,
+    marginHorizontal: 10,
+    marginVertical: 5,
+  }
+});
