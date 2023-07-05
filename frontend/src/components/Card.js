2e63f1cb5a861e8db3e1902e2aff72088379c889
dudcks5477 <dudcks5477@naver.com>
2023-07-05 16:55:25 +0900

    Add Image Placeholder(need more)

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import {View, Image, Text} from 'react-native';
 import PropTypes from 'prop-types';
+import placeholderImage from '../assets/party1.jpeg';
 import {styles} from '../styles/CardStyle';
 
 const Card = ({partyData}) => {
   const {imageDetails = [], partyName, content, partyDateTime, partyLocation, time, address} = partyData;
 
   const imageUrl = imageDetails.length > 0 ? imageDetails[0].uri : null;
-  console.log('dd', imageUrl);
+  console.log('dd', imageUrl); // 완성되면 지우기
   
 
   return (
     <View style={styles.cardContainer}>
       <View style={styles.card}>
-        {imageUrl && 
+        {imageUrl ? ( 
           <Image
             source={{uri: imageUrl}}
             style={styles.image}
           />
+        ) : (
+          // placholder 이미지를 여기에 추가 할 수 있다.
+          <Image
+            source={placeholderImage}
+            style={styles.image}
+          />
+        )
         }
         <Text style={styles.title}>{partyName}</Text>
         <Text style={styles.description}>{content}</Text>
