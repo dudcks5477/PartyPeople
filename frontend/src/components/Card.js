aec1d5a3c8a8f4db45bae542d0809b34828e3956
dudcks5477 <dudcks5477@naver.com>
2023-07-06 14:20:07 +0900

    Component seperate

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import {View, Image, Text} from 'react-native';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react';
+import {View, Image, ActivityIndicator} from 'react-native';
 import placeholderImage from '../assets/party1.jpeg';
+import PropTypes from 'prop-types';
 import {styles} from '../styles/CardStyle';
+import CardInfo from './CardInfo';
 
 const Card = ({partyData}) => {
-  const {imageDetails = [], partyName, content, partyDateTime, partyLocation, time, address} = partyData;
+
+  // For image loading
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {imageDetails = [], partyName, content} = partyData;
 
   const imageUrl = imageDetails.length > 0 ? imageDetails[0].uri : null;
+  
   console.log('dd', imageUrl); // 완성되면 지우기
   
 
@@ -15,24 +21,29 @@ const Card = ({partyData}) => {
     <View style={styles.cardContainer}>
       <View style={styles.card}>
         {imageUrl ? ( 
-          <Image
-            source={{uri: imageUrl}}
-            style={styles.image}
-          />
+          <>
+            <Image
+              source={{uri: imageUrl}}
+              style={styles.image}
+              onLoadStart={() => setIsLoading(true)}
+              onLoadEnd={() => setIsLoading(false)}
+            />
+            {isLoading && <ActivityIndicator style={styles.loadingIndicator} />}
+          </>
         ) : (
           // placholder 이미지를 여기에 추가 할 수 있다.
           <Image
             source={placeholderImage}
             style={styles.image}
           />
-        )
-        }
-        <Text style={styles.title}>{partyName}</Text>
-        <Text style={styles.description}>{content}</Text>
-        <Text style={styles.title}>{partyDateTime}</Text>
-        <Text style={styles.title}>{partyLocation}</Text>
-        <Text style={styles.title}>{time}</Text>
-        <Text style={styles.title}>{address}</Text>
+        )}
+        <CardInfo 
+          title={partyName} 
+          content={content}
+          partyLocation={partyData.partyLocation}
+          time={partyData.time}
+          address={partyData.address}  
+        /> 
       </View>
     </View>
   );
@@ -47,7 +58,6 @@ Card.propTypes = {
     ),
     partyName: PropTypes.string,
     content: PropTypes.string,
-    partyDateTime: PropTypes.string,
     partyLocation: PropTypes.string,
     time: PropTypes.string,
     address: PropTypes.string,
