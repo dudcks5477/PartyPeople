7896c5a22b69e066ecf93af042054b6d3f8c16be
dudcks5477 <dudcks5477@naver.com>
2023-07-06 14:33:00 +0900

    acecessibility image placeholder

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import {View, Image, ActivityIndicator} from 'react-native';
 import placeholderImage from '../assets/party1.jpeg';
 import PropTypes from 'prop-types';
+import { format } from 'date-fns';
+
 import {styles} from '../styles/CardStyle';
 import CardInfo from './CardInfo';
 
@@ -10,11 +12,19 @@ const Card = ({partyData}) => {
   // For image loading
   const [isLoading, setIsLoading] = useState(false);
 
-  const {imageDetails = [], partyName, content} = partyData;
-
-  const imageUrl = imageDetails.length > 0 ? imageDetails[0].uri : null;
+  const extractPartyData = partyData => {
+    const {imageDetails = [], partyName, content} = partyData;
+    const imageUrl = imageDetails.length > 0 ? imageDetails[0].uri : null;
+    return { imageUrl, partyName, content};
+  };
   
-  console.log('dd', imageUrl); // 완성되면 지우기
+  const {imageUrl, partyName, content} = extractPartyData(partyData);
+
+  // Format the party date
+  const formattedDate = partyData.partyDateTime ? format(new Date(partyData.partyDateTime), 'MMMM d, yyyy h:mm a') : null;
+  
+  // 완성되면 지우기
+  console.log('dd', imageUrl); 
   
 
   return (
@@ -23,16 +33,24 @@ const Card = ({partyData}) => {
         {imageUrl ? ( 
           <>
             <Image
+              accessible={true}
+              accessibilityLabel='Party Image'
               source={{uri: imageUrl}}
               style={styles.image}
               onLoadStart={() => setIsLoading(true)}
               onLoadEnd={() => setIsLoading(false)}
+              onError={(error) => {
+                // Handle image loading error
+                console.log('Image Loading error:', error);
+              }}
             />
             {isLoading && <ActivityIndicator style={styles.loadingIndicator} />}
           </>
         ) : (
           // placholder 이미지를 여기에 추가 할 수 있다.
           <Image
+            accessible={true}
+            accessibilityLabel='Placeholder Image'
             source={placeholderImage}
             style={styles.image}
           />
@@ -41,7 +59,7 @@ const Card = ({partyData}) => {
           title={partyName} 
           content={content}
           partyLocation={partyData.partyLocation}
-          time={partyData.time}
+          time={formattedDate} // formatting date relay
           address={partyData.address}  
         /> 
       </View>
@@ -61,6 +79,7 @@ Card.propTypes = {
     partyLocation: PropTypes.string,
     time: PropTypes.string,
     address: PropTypes.string,
+    partyDateTime: PropTypes.string,
   })
 };
 
