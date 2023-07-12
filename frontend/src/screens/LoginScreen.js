6158fdf9603dbcc84488ba0467b379695a892a2d
dudcks5477 <dudcks5477@naver.com>
2023-07-12 19:31:05 +0900

    Comment about GOOGLE Login

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,77 +1,27 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {View, Text, TouchableOpacity, Image} from 'react-native';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import NaverLogin, {getProfile as getNaverProfile} from '@react-native-seoul/naver-login';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
 
 import {styles} from '../styles/LoginStyle';
 import naverIcon from '../assets/naverLogo.png';
 import googleIcon from '../assets/googleLogo.png';
+// import useGoogleSignIn from '../hooks/useGoogleSignIn';
+import { useNaverSignIn } from '../hooks/useNaverSignIn';
 
-GoogleSignin.configure({
-  webClientId: '<My_WEB_CLIENT_ID>',
-  offlineAccess: true,
-});
-
-const initials = {
-  kConsumerKey: '<My_naver_app_key>',
-  kConsumerSecret: '<My_naver_app_secret>',
-  kServiceAppName: '<My_service_app_name>',
-  kServiceAppUrlScheme: '<My_service_app_url_scheme>' // only for ios
-};
-
-const LoginScreen = () => {
-  const [isSignInProgress, setIsSignInProgress] = useState(false);
-  const [naverToken, setNaverToken] = useState(null);
+const LoginScreen = () => { 
   const navigation = useNavigation();
 
-  const signInWithGoogle = async () => {
-    try {
-      setIsSignInProgress(true);
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signInWithGoogle();
-      console.log(userInfo);
-    } catch(error) {
-      console.log(error);
-    } finally {
-      setIsSignInProgress(false);
-    }
-  };
-
-  const signInWithNaver = async () => {
-    try {
-      setIsSignInProgress(true);
-      const result = await NaverLogin.login(initials);
-      if (result.accessToken) {
-        setNaverToken(result.accessToken);
-        getNaverUserInfo(result.accessToken);
-      } else {
-        console.log(result);
-      }
-    } catch(error) {
-      console.log(error);
-    } finally {
-      setIsSignInProgress(false);
-    }
-  };
-
-  const getNaverUserInfo = async (token) => {
-    try {
-      const profileResult = await getNaverProfile(token);
-      console.log(profileResult);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  // const {signInWithGoogle, isSignInProgress: isGoogleSignInProgress} = useGoogleSignIn();
+  const {signInWithNaver, isSignInProgress: isNaverSignInProgress} = useNaverSignIn();
 
   return (
     <View style={styles.container}>
       <Text style={styles.appName}>Party UP</Text>
       <TouchableOpacity
         style={[styles.signInButton, styles.allButton]}
-        onPress={signInWithGoogle}
-        disabled={isSignInProgress}
+        // onPress={signInWithGoogle}
+        // disabled={isGoogleSignInProgress}
       >
         <Image source={googleIcon} style={styles.icon} />
         <Text style={styles.buttonText}>Sign in With Google</Text>
@@ -79,7 +29,7 @@ const LoginScreen = () => {
       <TouchableOpacity
         style={[styles.signInButton, styles.allButton]}
         onPress={signInWithNaver}
-        disabled={isSignInProgress}
+        disabled={isNaverSignInProgress}
       >
         <Image source={naverIcon} style={styles.icon} />
         <Text style={styles.buttonText}>Sign in With Naver</Text>
@@ -93,7 +43,6 @@ const LoginScreen = () => {
       </TouchableOpacity>
     </View>
   );
-
 };
 
 export default LoginScreen;
