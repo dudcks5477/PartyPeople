79f1a83703cc028121afe4a7409533595c14f6fe
dudcks5477 <dudcks5477@naver.com>
2023-07-14 14:09:12 +0900

    Setting to eslint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const passport = require("passport");
+const passport = require('passport');
 
 router.get('/auth/google',
   passport.authenticate('google', { scope: ['profile']})
