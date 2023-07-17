b038c057aa2f3dec70df71eac0797b4397dd12b3
dudcks5477 <dudcks5477@naver.com>
2023-07-17 16:30:08 +0900

    Add Agreement Screen

--- frontend/App.js
+++ frontend/App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import LoginScreen from './src/screens/LoginScreen';
+import AgreementScreen from './src/screens/Auth/AgreementScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import UnderBarButton from './src/navigation/UnderBarButton';
 import MapScreen from './src/screens/MapScreen';
@@ -19,6 +20,10 @@ const App = () => (
       <Stack.Screen 
         name="Login" 
         component={LoginScreen}
+      />
+      <Stack.Screen
+        name="Agreement"
+        component={AgreementScreen}
       />
       <Stack.Screen
         name="Home"
