5e8115389a81ac8f63976530cedcdd7192ad2c0b
dudcks5477 <dudcks5477@naver.com>
2023-07-17 16:29:38 +0900

    Add background Image and connect with backend

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, Image} from 'react-native';
+import {View, Text, TouchableOpacity, Image, ImageBackground} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
@@ -24,8 +24,9 @@ const LoginScreen = () => {
       await GoogleSignin.hasPlayServices();
       const userInfo = await GoogleSignin.signIn();
       console.log(userInfo);
-    } catch (error) {
-      console.log(error);
+      navigation.navigate('Agreement');
+    } catch(error) {
+      console.log('Error:', error);
     } finally {
       setGoogleSignInProgress(false);
     }
@@ -34,32 +35,34 @@ const LoginScreen = () => {
   const {signInWithNaver, isSignInProgress: isNaverSignInProgress} = useNaverSignIn();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.appName}>Party UP</Text>
-      <TouchableOpacity
-        style={[styles.signInButton, styles.allButton]}
-        onPress={signInWithGoogle}
-        disabled={isGoogleSignInProgress}
-      >
-        <Image source={googleIcon} style={styles.icon} />
-        <Text style={styles.buttonText}>Sign in With Google</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[styles.signInButton, styles.allButton]}
-        onPress={signInWithNaver}
-        disabled={isNaverSignInProgress}
-      >
-        <Image source={naverIcon} style={styles.icon} />
-        <Text style={styles.buttonText}>Sign in With Naver</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[styles.signInButton, styles.allButton]}
-        onPress={() => navigation.navigate('BottomTab', {screen: 'Home'})}
-      >
-        <Icon name="user-o" size={20} color="#000"/>
-        <Text style={styles.buttonText}>Guest</Text>
-      </TouchableOpacity>
-    </View>
+    <ImageBackground source={require('../assets/LoginParty.jpg')} style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <Text style={styles.appName}>Party UP</Text>
+        <TouchableOpacity
+          style={[styles.signInButton, styles.allButton]}
+          onPress={signInWithGoogle}
+          disabled={isGoogleSignInProgress}
+        >
+          <Image source={googleIcon} style={styles.icon} />
+          <Text style={styles.buttonText}>Sign in With Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.signInButton, styles.allButton]}
+          onPress={signInWithNaver}
+          disabled={isNaverSignInProgress}
+        >
+          <Image source={naverIcon} style={styles.icon} />
+          <Text style={styles.buttonText}>Sign in With Naver</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.signInButton, styles.allButton]}
+          onPress={() => navigation.navigate('BottomTab', {screen: 'Home'})}
+        >
+          <Icon name="user-o" size={20} color="#000"/>
+          <Text style={styles.buttonText}>Guest</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 };
 
