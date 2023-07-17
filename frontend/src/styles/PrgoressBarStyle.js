a1fc40ee9128680c339619debfd07ddb27b0557a
dudcks5477 <dudcks5477@naver.com>
2023-07-17 16:31:34 +0900

    Add Progress Bar

---
+++ frontend/src/styles/PrgoressBarStyle.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export const progressStyles = (progress) => StyleSheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    marginTop: 10, 
+    marginBottom: 10
+  },
+  emptyBar: {
+    flexDirection: 'row', 
+    height: 5, 
+    width: '90%', 
+    backgroundColor:'#d2d2d2'
+  },
+  fillBar: {
+    backgroundColor: '#B39DDB', 
+    width: `${progress}%`
+  }
+});
