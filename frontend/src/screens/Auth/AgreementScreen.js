c5fab53f84db052f6371dced0712c935ddd46874
dudcks5477 <dudcks5477@naver.com>
2023-07-18 10:57:14 +0900

    Changed CamelCase about File name

--- frontend/src/screens/Auth/AgreementScreen.js
+++ frontend/src/screens/Auth/AgreementScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, ScrollView} from 'react-native';
+import {View, Text} from 'react-native';
 
 import ProgressBar from '../../components/ProgressBar';
 
