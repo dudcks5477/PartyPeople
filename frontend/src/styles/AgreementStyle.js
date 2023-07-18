8d5af083499f096e9629e5b94b234aac5acb2521
dudcks5477 <dudcks5477@naver.com>
2023-07-18 16:52:36 +0900

    Add google Font

---
+++ frontend/src/styles/AgreementStyle.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    width: '100%',
+    marginLeft: '5%',
+    marginRight: '5%',
+  },
+  title: {
+    fontSize: 30,
+    fontFamily: 'GamjaFlower-Regular',
+  },
+  subTitle: {
+    marginTop: 12,
+    marginBottom: 50,
+    fontSize: 12,
+  }
+});
