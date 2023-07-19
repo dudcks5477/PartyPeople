d7710facca87c8c1e3fa2bc04a6f633702020731
dudcks5477 <dudcks5477@naver.com>
2023-07-19 14:51:15 +0900

    Styles

--- frontend/src/styles/AgreementStyle.js
+++ frontend/src/styles/AgreementStyle.js
@@ -3,8 +3,6 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
     width: '100%',
     marginLeft: '5%',
     marginRight: '5%',
@@ -17,5 +15,41 @@ export const styles = StyleSheet.create({
     marginTop: 12,
     marginBottom: 50,
     fontSize: 12,
+  },
+  subContainer: {
+    flex: 1
+  },
+  agreements: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    marginBottom: 10
+  },
+  importText: {
+    color: 'red',
+    fontSize: 15
+  },
+  textAgreements: {
+    fontSize: 15
+  },
+  btnContainer: {
+    alignSelf: 'center',
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  outBtn: {
+    alignItems: 'center', 
+    justifyContent: 'center',
+    backgroundColor: '#DDD', 
+    padding: 10, 
+    marginRight: 20,
+    width: 100
+  },
+  inBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#DDD',
+    padding: 10,
+    width: 100,
+    marginRight: 40,
   }
 });
