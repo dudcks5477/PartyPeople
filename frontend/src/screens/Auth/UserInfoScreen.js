447a9919ac42088155b4a8dec27047d308b7b078
dudcks5477 <dudcks5477@naver.com>
2023-07-19 14:37:06 +0900

    Add User Info Screen

---
+++ frontend/src/screens/Auth/UserInfoScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const UserInfoScreen = () => {
+
+  return (
+    <View>
+      <Text>UserInfoScreen</Text>
+    </View>
+  );
+};
+
+export default UserInfoScreen;
