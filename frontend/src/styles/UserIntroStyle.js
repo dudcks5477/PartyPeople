0001e449eac6954e94cb76e2c6876a786348d9f0
dudcks5477 <dudcks5477@naver.com>
2023-07-28 16:39:19 +0900

    Add input Container style

---
+++ frontend/src/styles/UserIntroStyle.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    width: '90%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  input: {
+    height: 250,
+    width: 298,
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    fontSize: 13,
+  }
+});
