9761eab577f78b359f2e3de839a6a1e2c6278f17
dudcks5477 <dudcks5477@naver.com>
2023-07-28 15:57:03 +0900

    Created User Intro screen

---
+++ frontend/src/screens/Auth/UserIntro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const UserIntro = () => {
+  
+  return(
+    <View>
+      <Text>UserIntro</Text>
+    </View>
+  );
+};
+
+export default UserIntro;
