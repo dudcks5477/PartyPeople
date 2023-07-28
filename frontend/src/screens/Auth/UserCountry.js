47a56630b3eea49a39909e7c07399f0634633dcf
dudcks5477 <dudcks5477@naver.com>
2023-07-28 15:56:42 +0900

    Add Button

--- frontend/src/screens/Auth/UserCountry.js
+++ frontend/src/screens/Auth/UserCountry.js
@@ -1,15 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, TouchableOpacity, FlatList} from 'react-native';
 import { SearchBar } from 'react-native-elements';
+import { RadioButton } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
+import countries from '../../data/countries.json';
 import ProgressBar from '../../components/ProgressBar';
+import Button from '../../components/Button';
 import {styles as agreementStyles} from '../../styles/AgreementStyle';
-
-const countries = ['Korea', 'Japan', 'China', 'United States', 'Canada', 'Australia'];
+import {styles as userCountryStyles} from '../../styles/UserCountryStyle';
 
 const UserCountry = () => {
   const [search, setSearch] = useState('');
   const [filteredCountries, setFilteredCountries] = useState(countries);
+  const [selectedCountry, setSelectedCountry] = useState('');
+
+  const navigation = useNavigation();
 
   const handleSearch = (text) => {
     setSearch(text);
@@ -17,10 +23,21 @@ const UserCountry = () => {
     setFilteredCountries(newFilteredCountries);
   };
 
+  const handleSelectCountry = (country) => {
+    setSelectedCountry(country);
+  };
+
   const renderItem = ({item}) => {
-    <TouchableOpacity onPress={() => console.log(item)}>
-      <Text>{item}</Text>
-    </TouchableOpacity>;
+    return (
+      <TouchableOpacity style={userCountryStyles.item} onPress={() => console.log(item)}>
+        <RadioButton
+          title={item}
+          status={ selectedCountry === item ? 'checked' : 'unchecked' }
+          onPress={() => handleSelectCountry(item)}
+        />
+        <Text style={userCountryStyles.text}>{item}</Text>
+      </TouchableOpacity>
+    );
   };
   
   return (
@@ -32,6 +49,8 @@ const UserCountry = () => {
         <Text style={agreementStyles.subTitle}>이후 변경할 수 없으니 정확히 선택해주세요.</Text>
 
         <SearchBar
+          containerStyle={userCountryStyles.searchBarContainer}
+          inputContainerStyle={userCountryStyles.searchBarInputContainer}
           placeholder='국적 검색'
           onChangeText={handleSearch}
           value={search}
@@ -43,6 +62,23 @@ const UserCountry = () => {
           renderItem={renderItem}
           keyExtractor={item => item}
         />
+
+        <View
+          style={[agreementStyles.btnContainer, { paddingTop: 10 }]}
+        >
+          <Button 
+            text='나가기'
+            onPress={() => navigation.navigate('UserPhoto')}
+            style={agreementStyles.outBtn}
+          />
+          <Button
+            text='다음'
+            onPress={() => navigation.navigate('UserIntro')}
+            style={[agreementStyles.inBtn, {
+              backgroundColor: selectedCountry ? '#B39DDB' : '#fff'
+            }]}
+          />
+        </View>
 
       </View>
     </View>
