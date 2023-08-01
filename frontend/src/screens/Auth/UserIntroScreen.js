ed2a450a4e7d8f07032f7eb9457c9d1a3ea50ce2
dudcks5477 <dudcks5477@naver.com>
2023-08-01 15:57:40 +0900

    Fix Button

--- frontend/src/screens/Auth/UserIntroScreen.js
+++ frontend/src/screens/Auth/UserIntroScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, ScrollView, KeyboardAvoidingView, Platform} from 'react-native';
+import {View, Text, TextInput, ScrollView} from 'react-native';
 
 import ProgressBar from '../../components/ProgressBar';
 import Button from '../../components/Button';
@@ -13,57 +13,54 @@ const UserIntro = () => {
   const navigation = useNavigation();
   
   return(
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={{ flex: 1 }}
-    >
-      <ScrollView style={agreementStyles.background}
-        contentContainerStyle={{flex: 1}}
+    <View style={{flex: 1}}>
+      <ScrollView 
+        contentContainerStyle={{ flexGrow: 1 }} 
+        keyboardShouldPersistTaps="handled"
+        style={agreementStyles.background}
       >
-        <View style={{...agreementStyles.container, flex: 1, justifyContent: 'space-between'}}>
-          <View>
-            <ProgressBar progress={100}/>
+        <View style={agreementStyles.container}>
+          <ProgressBar progress={100}/>
 
-            <Text style={agreementStyles.title}>자기소개를</Text>
-            <Text style={agreementStyles.title}>입력해주세요</Text>
-            <Text style={agreementStyles.subTitle}>나를 소개할 수 있는 내용을 자유롭게 적어주세요.</Text>
+          <Text style={agreementStyles.title}>자기소개를</Text>
+          <Text style={agreementStyles.title}>입력해주세요</Text>
+          <Text style={agreementStyles.subTitle}>나를 소개할 수 있는 내용을 자유롭게 적어주세요.</Text>
 
-            <View style={userIntroStyles.inputContainer}>
-              <TextInput
-                style={userIntroStyles.input}
-                onChangeText={setText}
-                value={text}
-                textAlignVertical='top'
-                placeholder='예) 관심사, 취미, 좋아하는 노래 등'
-                placeholderTextColor='gray'
-                multiline
-              />
-            </View>
+          <View style={userIntroStyles.inputContainer}>
+            <TextInput
+              style={userIntroStyles.input}
+              onChangeText={setText}
+              value={text}
+              textAlignVertical='top'
+              placeholder='예) 관심사, 취미, 좋아하는 노래 등'
+              placeholderTextColor='gray'
+              multiline
+            />
+          </View>
 
-            <Text style={agreementStyles.subTitle}>
-              {`다른 메신저의 아이디를 기재할 경우 사기, 변태, 욕설 등의 위험에 노출될
+          <Text style={agreementStyles.subTitle}>
+            {`다른 메신저의 아이디를 기재할 경우 사기, 변태, 욕설 등의 위험에 노출될
 수 있습니다.`}</Text>
-          </View>
-          
-          <View
-            style={agreementStyles.btnContainer}
-          >
-            <Button 
-              text='나가기'
-              onPress={() => navigation.navigate('UserPhoto')}
-              style={agreementStyles.outBtn}
-            />
-            <Button
-              text='완료'
-              onPress={() => navigation.navigate('Home')}
-              style={[agreementStyles.inBtn, {
-                backgroundColor: '#B39DDB'
-              }]}
-            />
-          </View>
+        </View>
+
+        <View
+          style={[agreementStyles.btnContainer, {marginLeft: 30}]}
+        >
+          <Button 
+            text='나가기'
+            onPress={() => navigation.navigate('UserPhoto')}
+            style={agreementStyles.outBtn}
+          />
+          <Button
+            text='완료'
+            onPress={() => navigation.navigate('Home')}
+            style={[agreementStyles.inBtn, {
+              backgroundColor: '#B39DDB'
+            }]}
+          />
         </View>
       </ScrollView>
-    </KeyboardAvoidingView>
+    </View>
   );
 };
 
